d7a806c03c4971a395e495aea2b043004b207aee
Inha Tsishevska <afesh.ini@gmailcom>
2018-06-02 20:30:11 +0300

    Add DZ1 for lesson2

---
+++ lesson2/DZ1/src/main/java/DZ1.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.*;
+
+public class DZ1 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = initChromeDriver();
+        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+           driver.findElement(By.id("email")).sendKeys("webinar.test@gmail.com");
+           driver.findElement(By.id("passwd")).sendKeys("Xcg7299bnSmMuRLp9ITw");
+           driver.findElement(By.name("submitLogin")).click();
+           Thread.sleep(1000);
+           driver.findElement(By.id("header_employee_box")).click();
+           Thread.sleep(1000);
+           driver.findElement(By.id("header_logout")).click();
+
+    }
+
+    public static WebDriver initChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", DZ1.class.getResource("chromedriver.exe").getPath());
+        return new ChromeDriver();
+    }
+}
